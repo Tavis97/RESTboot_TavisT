4a1e1aff9f60e02103220ea3c5ec8ee4b61c4a02
Tavis97 <Tavis.Thompson@morehouse.edu>
2018-12-03 17:01:04 -0600

    Finish adding annotations

--- src/main/java/org/morehouse/app/resources/TodosResource.java
+++ src/main/java/org/morehouse/app/resources/TodosResource.java
@@ -42,11 +42,14 @@ public class TodosResource {
 	}
 	
 	// TODO: Supply the annotation for a GET request
+	@GET
 	// TODO: Supply the path annotation so that the todo id 
 	//       can be passed as parameter in the URL
+	@Path("/{id}")
 	// TODO: Supply the annotation so that the Todo returned by
 	//       this method is converted to either JSON or XML, 
 	//       depending on client request. 
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
 	// To test: http://localhost:9090/morehouse/restapp/todos/1
 	//   HTTP method: GET
 	// Headers:
@@ -59,10 +62,13 @@ public class TodosResource {
 	}
 	
 	// TODO: Supply the annotation for a POST request
+	@POST
 	// TODO: Supply the annotation so that the client passes a JSON
 	//       object in the body of the POST request
+	@Consumes(MediaType.APPLICATION_JSON)
 	// TODO: Supply the annotation so that the Todo returned by
 	//       this method is converted to a JSON object
+	@Produces(MediaType.APPLICATION_JSON)
 	// To test: http://localhost:9090/morehouse/restapp/todos
 	// HTTP method: POST
 	// Body:  {"title":"Of Mice and Men", "status":"IN", "dueDate":"2018-02-12", "comment":"checked-in ontime", "assignee":"Jimmy Wong"}
@@ -71,9 +77,6 @@ public class TodosResource {
 	// Headers:
 	//       Content-Type: application/json
 	//       Accept: application/json
-	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
 	
 	public Response createTodo(Todo todo) {
 		// Save the todo in the database
@@ -88,10 +91,13 @@ public class TodosResource {
 	}
 	
 	// TODO: Supply the annotation for a PUT request
+	@PUT
 	// TODO: Supply the annotation so that the client passes a JSON
 	//       object in the body of the PUT request
+	@Consumes(MediaType.APPLICATION_JSON)
 	// TODO: Supply the annotation so that the Todo returned by
 	//       this method is converted to a JSON object
+	@Produces(MediaType.APPLICATION_JSON)
 	// To test: http://localhost:9090/morehouse/restapp/todos
 	// HTTP method: PUT
 	// Body:  {"id":8, "title":"Of Mice and Men", "status":"IN", "dueDate":"2018-03-17", "comment":"checked-in ontime", "assignee":"Dana Robinson"}
@@ -109,10 +115,13 @@ public class TodosResource {
 	}
 	
 	// TODO: Supply the annotation for a DELETE request
+	@DELETE
     // TODO: Supply the path annotation so that the todo id 
 	//       can be passed as parameter in the URL
+	@Path("/{id}")
 	// TODO: Supply the annotation so that the Todo returned by
 	//       this method is converted to a JSON object
+	@Produces(MediaType.APPLICATION_JSON)
 	// To test: http://localhost:9090/morehouse/restapp/todos/<todo id>
 	// HTTP method: DELETE
 	// Headers:
